05acb926314ef1718f81d2bdef6cb8847641e4c5
Kishore.B <115468744+KishoreB7@users.noreply.github.com>
2024-08-12 19:22:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+# from langchain.llms import CTransformers
+from langchain_google_genai import GoogleGenerativeAI
+
+## Function To get response from Gemini
+
+def geminiresponse(input_text,no_words,blog_style):
+    
+    llm = GoogleGenerativeAI(google_api_key="YOUR API",model="gemini-1.5-pro")
+
+    template="""
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words.
+            """
+    
+    prompt=PromptTemplate(input_variables=["blog_style","input_text",'no_words'],
+                          template=template)
+    
+    ## Generate the ressponse from the gemini
+    response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=no_words))
+    print(response)
+    return response
+
+
+
+
+
+
+st.set_page_config(page_title="Generate Blogs",
+                    page_icon='🤖',
+                    layout='centered',
+                    initial_sidebar_state='collapsed')
+
+st.header("Generate Blogs 🤖")
+
+input_text=st.text_input("Enter the Blog Topic")
+
+## creating to more columns for additonal 2 fields
+
+col1,col2=st.columns([5,5])
+
+with col1: 
+    no_words=st.text_input('No of Words')
+with col2:
+    blog_style=st.selectbox('Writing the blog for',
+                            ('Researchers','Data Scientist','Common People'),index=0)
+    
+submit=st.button("Generate")
+
+## Final response
+if submit:
+    st.write(geminiresponse(input_text,no_words,blog_style))
